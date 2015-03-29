a784f2e3a65ef108541e30e03f98263164e7ac41
Albert Liang <albertliangcode@gmail.com>
2015-03-28 18:13:45 -0700

    Update README and create rpn.c

---
+++ README.md
@@ -0,0 +1,8 @@
+Reverse Polish Calculator
+
+3/28/15
+Albert Liang
+
+A baremetal implementation of a Reverse Polish Calculator for the Raspberry Pi.
+Hooks up to a framebuffer project I created for my CS107E class to display the state of the calculator on a screen.
+
---
+++ rpn.c
@@ -0,0 +1,14 @@
+/**
+* Reverse Polish Calculator - Baremetal Raspberry Pi
+*
+* 3/28/15
+* Albert Liang
+* 
+* This program simply takes in a string and recursively parses it to get a
+* result. The result is returned as a string, and printed to Screen.
+* 
+* After this is working, make this program should become executable in the 
+* Framebuffer console.
+**/
+
+
